3addfa7a13e46ea588d69e952d60993347e24062
barbosaisa <106408514+barbosaisa@users.noreply.github.com>
2023-06-19 20:02:47 -0300

    Add files via upload

---
+++ atv-dom-2-main/Main.js
@@ -0,0 +1,19 @@
+var titulo = window.document.getElementById('titulo')
+var resp_ti = prompt('insira o título:')
+titulo.innerText = resp_ti
+
+var titulo = window.document.getElementById('subtitulo')
+var resp_ti = prompt('insira o subtitulo:')
+titulo.innerText = resp_ti
+
+var titulo = window.document.getElementById('descricao')
+var resp_ti = prompt('insira uma descrição:')
+titulo.innerText = resp_ti
+
+var titulo = window.document.getElementById('lista')
+var resp_ti = prompt('Vamos fazer uma lista:')
+titulo.innerText = resp_ti
+
+var titulo = window.document.getElementById('lista')
+var resp_ti = prompt('Vamos fazer uma lista:')
+titulo.innerText = resp_ti
